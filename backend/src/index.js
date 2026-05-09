7f5720f222d06ea3900d8b0fe486e024b277bcfe
Aqsa <aqsaabdullah38403@gmail.com>
2026-05-09 17:34:14 +0500

    project

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://mern-chat-app-b9a3.vercel.app/",
     credentials: true,
   })
 );
