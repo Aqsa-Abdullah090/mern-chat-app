1af763cee9993465bce831fe9842fe31ec7dfe43
Aqsa <aqsaabdullah38403@gmail.com>
2026-05-11 20:32:37 +0500

    add

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,9 +2,13 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    // Fixed: Added quotes around the connection string
+    const conn = await mongoose.connect("mongodb+srv://aqsaabdullah38403_db_user:ePyDE7MwJXZgwgvf@cluster0.vqjeroh.mongodb.net/chat_db?retryWrites=true&w=majority");
+    
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.log("MongoDB connection error:", error);
+    // Exit process if DB connection fails
+    process.exit(1);
   }
 };
