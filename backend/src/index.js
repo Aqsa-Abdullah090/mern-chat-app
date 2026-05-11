771b1022521c4663300f620fc2843eb9a853dc9a
aqsa <aqsa@betacodespk.com>
2026-05-11 12:23:22 +0500

    chage

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://mern-chat-app-omega-puce.vercel.app",
     credentials: true,
   })
 );
